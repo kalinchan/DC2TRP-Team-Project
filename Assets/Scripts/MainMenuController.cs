ba272a8d1f9e2ac7a98f530f2440eebe9ec96caf
Kalin Chan <kalin.chan@payara.fish>
2023-02-07 14:10:22 +0000

    Main Menu and Options Menu creation

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
+    public void playGame()
+    {
+        SceneManager.LoadScene("BattleScene");
+    }
+
+    public void options()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void back()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
