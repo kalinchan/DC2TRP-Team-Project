e68cace349f87ef709de17272b71904ae943dfbf
TheCherubQueen <88330357+TheCherubQueen@users.noreply.github.com>
2023-02-15 18:10:28 +0000

    Enemy Logic, TurnManager, EndTurn, PlayerLogic
    finished setting up all logic scripts so that a turn can be taken, passed and taken by the opponent.
    
    enemies now attack/defend/special on their turn as many times as they have moves remaining.
    
    sorted player energy system, so that cards that cost too much energy cannot be played

--- Assets/Scripts/Logic/EnemyLogic.cs
+++ Assets/Scripts/Logic/EnemyLogic.cs
@@ -1,10 +1,13 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class EnemyLogic : MonoBehaviour
 {
-    public int damage, charge, maxCharge, defence, moves, maxMoves, attackCost, defenceCost, specialCost;
+    public int damage, charge, maxCharge, defence, movesRemaining, maxMoves, attackCost, defenceCost, specialCost;
     bool myTurn;
     private EntityStats self, player;
     
@@ -16,7 +19,7 @@ public class EnemyLogic : MonoBehaviour
         charge = 0;
         maxCharge = 4;
         defence = 2;
-        moves = 2;
+        movesRemaining = 2;
         maxMoves = 2;
         attackCost = 1;
         defenceCost = 1;
@@ -34,7 +37,7 @@ public class EnemyLogic : MonoBehaviour
     public void attack()
     {
         player.takeDamage(damage);
-        moves -= attackCost;
+        movesRemaining -= attackCost;
     }
 
     //use special attack
@@ -45,14 +48,14 @@ public class EnemyLogic : MonoBehaviour
 
         //the logic for charging this can be done with the turnCounter Value and calculating when there is no remainder when divided by a charge threshold? maybe.
 
-
+        movesRemaining-= specialCost;
     }
 
     //gain defence equal to defend value
     public void defend()
     {
         self.gainDefence(defence);
-        moves -= defenceCost;
+        movesRemaining -= defenceCost;
     }
 
     public void startTurn()
@@ -62,6 +65,9 @@ public class EnemyLogic : MonoBehaviour
     }
 
     //tells the TurnManager that this turn is done
+
+    //THIS WILL CAUSE ISSUES WITH MULTIPLE ENEMIES IN ONE LEVEL!!! Not tested yet but almost certain that because this method CHANGES the turn directly, it will mean that once one enemy takes its turn, no more enemies will be
+    //able too. as I say, not sure yet, but I can forsee it being an issue! Maybe we need to have something else that checks if ALL enemies myTurn == false and then call the method - probably safer. JD 14/02 <3
     public void endTurn()
     {
         myTurn = false;
@@ -70,6 +76,31 @@ public class EnemyLogic : MonoBehaviour
 
     public void turnTaker()
     {
+        movesRemaining = maxMoves;
+        charge++;
+        while(movesRemaining > 0)
+        {
+            int random = Random.Range(1, 3);
+            switch (random)
+            {
+                case 1:
+                    attack();
+                    break;
+                case 2:
+                    defend();
+                    break;
+                case 3:
+                    if(charge == maxCharge)
+                    {
+                        special();
+                        break;
+                    }
+                    attack();
+                    break;
+               
+            }
+        }
+        endTurn();
         //if myTurn = true this method runs
         //resets moves to max, increases charge by one
         //using as many moves as the enemy has available carry out, random
