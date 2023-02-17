1b0be2b081da8149c0b80a96b985e1bc64b9c0c5
cameron-barker-goodyear <101347655+cameron-barker-goodyear@users.noreply.github.com>
2023-02-17 17:27:04 +0000

    Random background when level starts

--- Assets/Scripts/LevelLoad.cs
+++ Assets/Scripts/LevelLoad.cs
@@ -8,7 +8,10 @@ public class LevelLoad : MonoBehaviour
 {
     public GameObject Card01, Card02, Card03, Card04, Card05, Card06, Card07, Card08, Card09, Card10, Card11, Card12;
     public GameObject PlayerArea;
-
+    public Sprite Background01, Background02, Background03;
+    public GameObject backgroundParent;
+    
+    List<Sprite> backgrounds = new List<Sprite>();
     List<GameObject> cards = new List<GameObject>();
 
 
@@ -27,6 +30,18 @@ public class LevelLoad : MonoBehaviour
             GameObject playerCard = Instantiate(cards[Random.Range(0, cards.Count)], new Vector3(0, 0, 0), Quaternion.identity);
             playerCard.transform.SetParent(PlayerArea.transform, false);
         }
+
+        //change background
+        backgrounds.AddRange(new List<Sprite>
+            {
+                Background01, Background02, Background03
+        }
+        );
+        int randomIndex = Random.Range(0, backgrounds.Count);
+
+        // Enable the background image at the random index
+        backgroundParent.transform.GetChild(randomIndex).gameObject.SetActive(true);
+
 
     }
 }
