922444f02bf4310636370140882e2ec2be4d928b
Bonsu, Shadrach Ofori (Capgemini) <shadrach.a.bonsu@capgemini.com>
2023-03-22 17:42:50 +0000

    DefenceBar player and enemy
    DefenceBar has been added and working for both player and enemy

---
+++ Assets/DefenceBarScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DefenceBarScript : MonoBehaviour
+{
+
+    public Slider slider;
+
+
+    public void SetMaxDefence(int defence)
+    {
+        slider.maxValue = defence;
+        slider.value = defence;
+    }
+
+    public void SetDefence(int defence)
+    {
+        slider.value = defence;
+    }
+}
